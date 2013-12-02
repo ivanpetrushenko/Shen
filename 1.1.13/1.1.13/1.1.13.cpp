fc3bcfe17375024351f57a70ddc1a57a263ff9d9
Ivan Petrushenko <ya.ipetrushenko@yandex.ru>
2013-12-03 00:45:43 +0200

    create the function

--- 1.1.13/1.1.13/1.1.13.cpp
+++ 1.1.13/1.1.13/1.1.13.cpp
@@ -2,10 +2,21 @@
 //
 
 #include "stdafx.h"
+#include <iostream>
 
+using namespace std;
 
-int _tmain(int argc, _TCHAR* argv[])
+// a, b > 0
+int gcd(int a, int b)
 {
+	if (b == 0) return a;
+	int r = a%b;
+	return gcd(b, r);
+}
+
+int main()
+{
+	cout << gcd(5,3)<< endl;
 	return 0;
 }
 
