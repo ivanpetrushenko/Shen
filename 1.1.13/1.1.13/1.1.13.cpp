2d4aa4d937a07c4642b51fc0adafae94cf522d4b
ivanpetrushenko <ya.ipetrushenko@yandex.ru>
2015-08-07 12:44:44 +0300

    Update 1.1.13.cpp

--- 1.1.13/1.1.13/1.1.13.cpp
+++ 1.1.13/1.1.13/1.1.13.cpp
@@ -1,35 +1,12 @@
-// 1.1.13.cpp: ���������� ����� ����� ��� ����������� ����������.
-//
 
-#include "stdafx.h"
 #include <iostream>
 
 using namespace std;
 
-// a, b > 0
-int gcd(int a, int b)
-{
-	if (b == 0) return a;
-	int r = a%b;
-	return gcd(b, r);
-}
-
-/*
-int gcd(int a, int b)
-{
-	if (b == 0) return a;
-	if (a == 0) return b;
-
-	if (a >= b)
-		return gcd(a%b, b);
-	if (b >= a)
-		return gcd(a, b%a);
-}
-*/
-
 int main()
 {
-	cout << gcd(5,3)<< endl;
+	cout << Самому ох*нному тім-ліду мої вітання з днем народження!
+	        Бережи себе, всього найкращого!) << endl;
 	return 0;
 }
 
